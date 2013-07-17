004d3c59b12602858faa368e5ccc5721fc87b1a5
Binney <sar.binney@hotmail.co.uk>
2013-07-17 20:34:07 +0100

    factorygirl problems

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -5,9 +5,19 @@ describe "User pages" do
   subject { page }
 
   describe "signup page" do
+    let(:user) { FactoryGirl.create(:user) }
     before { visit signup_path }
 
     it { should have_content('Sign up') }
     it { should have_title(full_title('Sign up')) }
   end
+
+end
+
+describe "profile page" do
+  # Replace with code to make a user variable
+  before { visit user_path(user) }
+
+  it { should have_content(user.name) }
+  it { should have_title(user.name) }
 end
